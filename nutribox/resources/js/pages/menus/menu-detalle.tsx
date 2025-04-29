78f6019e0ddce511490696d7b83719a70bf09e7e
sala_portatil <608222@alu.murciaeduca.es>
2025-04-30 00:39:57 +0200

    MENU DETALLE: Machos visuales con el componente react-chartjs-2

--- nutribox/resources/js/pages/menus/menu-detalle.tsx
+++ nutribox/resources/js/pages/menus/menu-detalle.tsx
@@ -1,12 +1,16 @@
 import { Button } from '@/components/ui/button';
 import AppLayout from '@/layouts/app-layout';
 import { Head, router } from '@inertiajs/react';
+import { ArcElement, Chart as ChartJS, Legend, Tooltip } from 'chart.js';
 import { useState } from 'react';
+import { Pie } from 'react-chartjs-2';
 import { toast } from 'sonner';
 import { ProductoTipo, columnsProductos } from './columnsProductos';
 import { DataTable } from './data-table';
 import { getIconoPorTipo } from './iconosMenu';
 import { getIconoPorTipo2 } from './iconosMenu2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 type ComidaConTotales = {
     id: number;
@@ -108,57 +112,82 @@ export default function VerDetalles({ menuSeleccionado, totalesComidas }: Props)
         }
     };
 
+    const graficaTotales = {
+        labels: ['Grasas', 'Hidratos de Carbono', 'Proteína'],
+        datasets: [
+            {
+                label: ' Gramos',
+                data: [Math.round(totalesMenu.gr), Math.round(totalesMenu.ch), Math.round(totalesMenu.pr)],
+                backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)'],
+                borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
+                borderWidth: 3,
+                hoverOffset: 19,
+                offset: 13,
+            },
+        ],
+    };
+    const graficaOpciones = {
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true,
+        },
+    };
+
     return (
         <AppLayout>
             <Head /*title={`Menú del ${menuSeleccionado.fecha}`}*/ />
-           
+
             <div className="relative flex h-full flex-1 flex-col gap-8 rounded-xl p-4">
-                 {/* Fondo */}
-                 <div className="absolute right-0 top-0 h-[100vh] w-[100vw] opacity-100 z-[0] pointer-events-none">
-        {getIconoPorTipo2(menuSeleccionado.tipo)}
-    </div>
+                {/* Fondo */}
+                <div className="pointer-events-none absolute top-0 right-0 z-[0] h-[100vh] w-[100vw] opacity-100">
+                    {getIconoPorTipo2(menuSeleccionado.tipo)}
+                </div>
                 {/* Modificacion nombre + Botón Eliminar menú */}
                 {/* Disparador de la función al salir del input con onBlur o la tecla Enter */}
-                <div className="flex items-center justify-between">
-                    {editando ? (
-                        <input
-                            className="w-full rounded border px-2 py-1 text-3xl uppercase"
-                            value={nombre}
-                            onChange={(e) => setNombre(e.target.value)}
-                            autoFocus
-                            onBlur={handleActualizarNombre}
-                            onKeyDown={(e) => {
-                                if (e.key === 'Enter') handleActualizarNombre();
-                            }}
-                        />
-                    ) : (
-                        <div className="group relative">
-                            <h1 className="cursor-pointer text-4xl hover:underline" onClick={() => setEditando(true)}>
-                                <div className="flex items-center gap-2">
-                                    {getIconoPorTipo(menuSeleccionado.tipo)} <small>Menú:</small>
-                                </div>{' '}
-                                <span className="uppercase">{nombre}</span>
-                            </h1>
-                            {/* Tooltip */}
-                            <span className="bg-muted-foreground absolute top-full left-0 mt-2 hidden w-max rounded px-2 py-1 text-xs text-white group-hover:block">
-                                Haz click aquí para renombrar el menú
-                            </span>
-                        </div>
-                    )}
 
-                    <Button className="cursor-pointer" variant="destructive" onClick={() => handleEliminar(menuSeleccionado.id)}>
-                        Eliminar
-                    </Button>
-                </div>
+                {editando ? (
+                    <input
+                        className="w-full rounded border px-2 py-1 text-3xl uppercase"
+                        value={nombre}
+                        onChange={(e) => setNombre(e.target.value)}
+                        autoFocus
+                        onBlur={handleActualizarNombre}
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter') handleActualizarNombre();
+                        }}
+                    />
+                ) : (
+                    <div className="group relative">
+                        <h1 className="cursor-pointer text-4xl hover:underline" onClick={() => setEditando(true)}>
+                            <div className="flex items-center gap-2">
+                                {getIconoPorTipo(menuSeleccionado.tipo)} <small>Menú:</small>
+                            </div>{' '}
+                            <span className="uppercase">{nombre}</span>
+                        </h1>
+                        {/* Tooltip */}
+                        <span className="bg-muted-foreground absolute top-full left-0 mt-2 hidden w-max rounded px-2 py-1 text-xs text-white group-hover:block">
+                            Haz click aquí para renombrar el menú
+                        </span>
+                    </div>
+                )}
 
+                    <div className="-mt-6 mb-0 pl-0  h-[150px] w-[150px]">
+                        <Pie data={graficaTotales} options={graficaOpciones} />
+                    </div>
                 <p className="-mt-6 mb-0 pl-0">
-                    Energía {Math.round(totalesMenu.kcal)} kcal &nbsp;&nbsp;|&nbsp;&nbsp; Grasas {Math.round(totalesMenu.gr)} g
+                    Energía {Math.round(totalesMenu.kcal)} kcal {/*  &nbsp;&nbsp;|&nbsp;&nbsp; Grasas {Math.round(totalesMenu.gr)} g
                     &nbsp;&nbsp;|&nbsp;&nbsp; Hidratos de Carbono {Math.round(totalesMenu.ch)} g &nbsp;&nbsp;|&nbsp;&nbsp; Proteínas{' '}
-                    {Math.round(totalesMenu.pr)} g
+                    {Math.round(totalesMenu.pr)} g */}
                 </p>
 
                 <p className="-mt-6 text-gray-600">Fecha: {menuSeleccionado.fecha}</p>
-
+              
+            
                 {/* Modificación info_extra */}
                 <div className="-mt-6">
                     {editandoInfoExtra ? (
@@ -221,9 +250,13 @@ export default function VerDetalles({ menuSeleccionado, totalesComidas }: Props)
                     </div>
                 ))}
             </div>
-            <div className="gap-8 p-8">
+
+            <div className="flex justify-between gap-8 p-8">
                 <Button className="cursor-pointer" variant="secondary" onClick={() => router.visit('/menuslistar')}>
                     ← Volver
+                </Button>
+                <Button className="cursor-pointer" variant="destructive" onClick={() => handleEliminar(menuSeleccionado.id)}>
+                    Eliminar
                 </Button>
             </div>
         </AppLayout>
